af870b4a667d65375c065b75ef349995fac1c921
mario-garcia-dev <garciasernamarioalberto@gmail.com>
2026-02-17 11:52:26 -0600

    ya?

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
         path: "prisma/migrations",
     },
     datasource: {
-        url: process.env["RAILWAY_URL"],
+        url: process.env["DATABASE_URL"],
     },
 });
